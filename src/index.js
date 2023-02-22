dfdd3c38cea92d0eab7ecc9b1d8b08fd2e220415
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-02-22 14:49:18 -0300

    Add font

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 import './style.css';
-
-function component() {
-  const element = document.getElementById('container');
-  element.classList.add('title');
-  element.textContent = 'test';
-  return element;
-}
-
-document.body.appendChild(component());
