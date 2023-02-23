f5c696927ea315285d270b4c96ce7fc73fea526b
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-02-22 22:50:21 -0300

    Fix lighthouse webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackLighthousePlugin = require('webpack-lighthouse-plugin');
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    new WebpackLighthousePlugin({
+      url: 'http://localhost:8080',
     }),
   ],
   mode: 'development',
